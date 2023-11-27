13daf651121be9f66075c47f7f3e6d505abe1244
MARCOS VINICIUS DA SILVA ROSA <rosa.3950@aluno.pr.senac.br>
2023-11-27 17:14:13 -0300

    firstcommit

---
+++ src/main/java/com/mycompany/jogos_mvh/Jogos_MvH.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jogos_mvh;
+
+/**
+ *
+ * @author rosa.3950
+ */
+public class Jogos_MvH {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
